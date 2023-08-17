a5ad21a7ade152fc41c5cd9cc7e4f8d67acdfa5c
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2023-08-17 15:49:22 +0800

    formarray usage example

--- reactive-forms-app/src/app/form-array/form-array.component.ts
+++ reactive-forms-app/src/app/form-array/form-array.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
+
+interface RecipeIngredient {
+  name: string,
+  quantity: string,
+  unit: string
+}
 
 @Component({
   selector: 'app-form-array',
@@ -6,5 +13,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-array.component.css']
 })
 export class FormArrayComponent {
+  recipeIngredients: RecipeIngredient[] = [];
+  selectIngredientsForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.selectIngredientsForm = this.fb.group({
+      recipeIngredients: this.fb.array([
+        this.buildFormGroup() 
+      ])
+    });
+  }
+  
+  buildFormGroup(): FormGroup {
+    return this.fb.group({
+      name: this.fb.control(''),
+      quantity: this.fb.control(''),
+      unit: this.fb.control(''),
+    });
+  }
+
+  getRecipeIngredientsArray(): FormArray {
+    return this.selectIngredientsForm.get('recipeIngredients') as FormArray;
+  }
+
+  // add formGroup to FormArray
+  pushToArray() {
+    const recipeIngredients = this.getRecipeIngredientsArray();
+    recipeIngredients.push(this.buildFormGroup());
+  }
+
+  // remove formGroup at given index from FormArray
+  removeAt(i: number) {
+    const recipeIngredients = this.getRecipeIngredientsArray();
+    recipeIngredients.removeAt(i);
+  }
+
+  onSubmit() {
+    const formData = this.getRecipeIngredientsArray().value;
+    this.recipeIngredients = [];
+    for (let index = 0; index < formData.length; index++) {
+      this.recipeIngredients.push(formData[index]);
+    }
+    console.log(this.recipeIngredients);
+  }
 }
